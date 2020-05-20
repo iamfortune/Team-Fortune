8885bc6124463de221bacb3de7d413c3ac8c7062
Uduak 'Eren <sandy3jnr@gmail.com>
2020-05-20 01:59:40 -0700

    Added coding challenge for today May 20, 2020

---
+++ daily-coding-challlenges/20-05-2020/lastIndexOf.js
@@ -0,0 +1,10 @@
+/*
+ *
+ * The string type in JavaScript has a lastIndexOf (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf) method. 
+ * Write your own implementation of this method in a function called myLastIndexOf. 
+ * The function should take in an array and a search value and work as intended.
+ */
+
+const myLastIndexOf = (arr, searchVal) => {
+	/* Your implementation goes here */
+}
