ba9225852c3e827e4f172a77c7a0c83de06d975a
Uduak 'Eren <sandy3jnr@gmail.com>
2020-05-21 07:32:27 -0700

    Added coding challenge for May 21, 2020

---
+++ daily-coding-challlenges/21-05-2020/indexOf.js
@@ -0,0 +1,10 @@
+/*
+ * The string type in JavaScript has an index (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf) method. 
+ * Write your own implementation of this method in a function called myIndex. 
+ * The function should take in an array and a search value and work as intended.
+ * 
+ * Example: myIndexOf([12, 8, -3, 4], -3) should return 2;
+*/
+const myIndexOf = (arr, searchVal) => {
+	/* Your implementation goes here */
+}
