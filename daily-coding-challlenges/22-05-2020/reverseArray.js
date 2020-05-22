c17020e2d382dab6962b9d66f562cf9062886d70
Uduak 'Eren <sandy3jnr@gmail.com>
2020-05-22 04:39:12 -0700

    Added coding challenge for May 22, 2020

---
+++ daily-coding-challlenges/22-05-2020/reverseArray.js
@@ -0,0 +1,7 @@
+/*
+ * Write a simple function to reverse the elements of an array
+ * Example: reverseArray([12, 8, -3, 4]) should return [4, -3, 8, 12];
+*/
+const reverseArray = (arr) => {
+	/* Your implementation goes here */
+}
