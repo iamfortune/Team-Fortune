bccd21fde3b0776ca79aa9793a0e721eb9975727
iamfortune <ikechifortune@gmail.com>
2020-05-28 09:33:40 +0100

    fixed it

--- daily-coding-challenges/26-05-2020/findGreatestNumInArray.js
+++ daily-coding-challenges/26-05-2020/findGreatestNumInArray.js
@@ -4,6 +4,7 @@
 */
 const findGreatestNumInArray = (arr) => {
 	/* Your implementation goes here */
+	if (arr.legnth ==1) {return arr[0]}
 	let maxNum = arr[0];
 	for(let i = 1; i < arr.length; i++) {
 		if(arr[i] > maxNum){
