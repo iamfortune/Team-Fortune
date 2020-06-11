eee2114a5d25e8790a34190dcc9ed574cf82a594
Uduak 'Eren <sandy3jnr@gmail.com>
2020-06-11 05:35:52 -0700

    Added coding challenge for June 11, 2020

---
+++ daily-coding-challenges/11-06-2020/reverseString.js
@@ -0,0 +1,18 @@
+/*
+ * Write a simple function to reverse a string
+ * Example: reverseString('Thisisgreat') should return 'taergsisihT';
+*/
+
+const reverseString = (str) => {
+	/* Your implementation goes here */
+
+}
+
+/** Sample Tests
+ * They should all print true if your code is correct
+ **/
+console.log(reverseString('Thisisgreat') === 'taergsisihT');
+console.log(reverseString('abc') === 'cba');
+console.log(reverseString('1209') === '9021');
+console.log(reverseString('') === '');
+
