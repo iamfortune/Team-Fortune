20ca70a3d89f9044f504a75747efaa7ab5ac3c00
iamfortune <ikechifortune@gmail.com>
2020-06-12 17:07:05 +0100

    worked!

--- daily-coding-challenges/11-06-2020/reverseString.js
+++ daily-coding-challenges/11-06-2020/reverseString.js
@@ -3,16 +3,23 @@
  * Example: reverseString('Thisisgreat') should return 'taergsisihT';
 */
 
-const reverseString = (str) => {
-	/* Your implementation goes here */
 
-}
+const reverseString = (str) => {
+  /* Your implementation goes here */
+  if (!str) {
+    return null;
+  }
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+};
 
 /** Sample Tests
  * They should all print true if your code is correct
  **/
-console.log(reverseString('Thisisgreat') === 'taergsisihT');
-console.log(reverseString('abc') === 'cba');
-console.log(reverseString('1209') === '9021');
-console.log(reverseString('') === '');
-
+console.log(reverseString("Thisisgreat") === "taergsisihT");
+console.log(reverseString("abc") === "cba");
+console.log(reverseString("1209") === "9021");
+console.log(reverseString("") === "");
