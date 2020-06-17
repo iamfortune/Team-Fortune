6077bef0898106329792a6fe5313120ae58f3aba
Uduak 'Eren <sandy3jnr@gmail.com>
2020-06-16 19:36:49 -0700

    Coding challenge for 17/06/2020

---
+++ daily-coding-challenges/17-06-2020/findLongestWordLength.js
@@ -0,0 +1,24 @@
+/*
+ * Source: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/find-the-longest-word-in-a-string
+ * 
+ * Write a simple function that takes a text and returns the length of the longest word in the text.
+ * Example
+ * -------
+ * findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6 
+ * Because the longest word 'jumped' and its length is 6;
+*/
+
+
+const findLongestWordLength = (str) => {
+	/* Your implementation goes here */
+	return 0;
+};
+
+/** Sample Tests
+ * They should all print true if your code is correct
+ **/
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog") === 6);
+console.log(findLongestWordLength("abc") === 3);
+console.log(findLongestWordLength("This this this") === 4);
+console.log(findLongestWordLength("") === 0);
+console.log(findLongestWordLength() === 0);
