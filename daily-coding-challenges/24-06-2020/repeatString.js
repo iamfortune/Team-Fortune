b05beacfb91116cb5fe4146514943ed6344acac2
Uduak 'Eren <sandy3jnr@gmail.com>
2020-06-23 22:51:29 -0700

    Added coding challenge for Wed Jun 24, 2020

---
+++ daily-coding-challenges/24-06-2020/repeatString.js
@@ -0,0 +1,24 @@
+/*
+ * Source: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/repeat-a-string-repeat-a-string
+ * 
+ * Write a simple function that repeats a given string str (first argument) for num times (second argument). 
+ * Return an empty string if num is not a positive number.
+ * 
+ * Example
+ * -------
+ * repeatStringNumTimes("*", 3) should return '***' 
+ * repeatStringNumTimes("abc", 1) should return 'abc'.
+*/
+
+
+const repeatStringNumTimes = (str, num) => {
+	/* Your implementation goes here */
+	return str;
+}
+
+/** Sample Tests
+ * They should all print true if your code is correct
+ **/
+console.log(repeatStringNumTimes('abc', 1) === 'abc');
+console.log(repeatStringNumTimes('*', 5) === '*****');
+console.log(repeatStringNumTimes('repeat', -1) === '');
